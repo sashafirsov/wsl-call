7c91ac1d80a1011ea21b752c22c45431178cbb0a
Sasha Firsov <suns@firsov.net>
2020-05-15 16:49:34 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# wsl-call
-Enabling Windows for Linux commands in windows
+# Enabling Windows for Linux commands in windows
+## setup
+1. create folder for linux executable symlinks
+2. download into folder binary wsl-call.exe from releases
+3. run `wsl-call.exe -all` to generate executables which match commands in linux PATH
+4. add folder to windows environment PATH
+
+## enable particular command
+If you want to use Linux shell commands like 'npm', the alias could be created by
+
+    wsl-call.exe ~/n/bin/node
+    wsl-call.exe ~/n/bin/npm
+
+or
+    wsl-call.exe /usr/bin/node
+    wsl-call.exe /usr/bin/npm
+
+without full path the linux PATH is used for command invoking
+
+    wsl-call.exe ifconfig
+
+The linux full path is preserved in `COMMAND.wslpath` next to command executable `COMMAND.exe`
---
+++ wsl-call.cpp
@@ -0,0 +1,91 @@
+#include <windows.h> 
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+typedef  char const* PCSTR;
+inline bool     isSeparator( char c ){ return c == '/' || c == '\\'; }
+inline char*    lastNameInPath( char* path )
+{
+    char* p = path + strlen(path) - 2;
+    for( ;p > path; p-- )
+        if( isSeparator(*p))
+            return ++p;
+    return path;
+}
+class string: public std::string
+{
+public:
+    string(std::string const& s) : std::string(s){}
+    operator const char* () { return c_str(); }
+    string operator + (const  char*  s) { return operator+( std::string(s) ); }
+    string operator + (string const& s) { return string( (std::string)(*this) + (std::string)s ); }
+};
+void writeFile( char const* fileName, char const* str )
+{
+    FILE* fptr = fopen( fileName, "w" );
+
+    if (fptr == NULL) {
+        printf("Error writing to file %s", fileName );
+        exit(1);
+    }
+    fprintf(fptr, "%s", str);
+    fclose(fptr);
+}
+inline std::string file2str(const char* path) {
+    std::ifstream input(path);
+    std::stringstream sstr;
+    while (input >> sstr.rdbuf());
+    return sstr.str();
+}
+string wslPath(string name)
+{
+    string fileName = name + ".wslpath";
+    return file2str(fileName);
+}
+int main( int argc, char* argv[], char* envp[] )
+{
+    PVOID OldValue = NULL;
+    Wow64DisableWow64FsRedirection(&OldValue); // needed only for 32 bit binary
+
+    string origName(lastNameInPath(argv[0]));
+    string name = origName.substr(0, origName.find_last_of('.'));
+
+    string winDir = string(getenv("SystemRoot"));
+    string cmd = winDir + "\\System32\\wsl.exe ";
+    if( strstr(argv[0], "wsl-call.exe") )
+    {
+        if (argc < 2)
+        {   printf("\
+Run `wsl-call.exe -all` to create windows EXE aliases for default linux instance binaries in current directory.\n\
+Do not forget to put this directory in windows PATH.\n\
+`wsl-call.exe <command>...` will create EXE alias for given one or more linux command.");
+            return 0;
+        }
+        if( strstr( argv[1], "-all" ) )
+        {   
+            printf("Please wait, listing could take a while.\n\n");
+
+            writeFile("wsl-call.sh", "compgen -ac|while read file; do fullname=`which $file` ; if ! [[ $fullname =~ /mnt/ ]]; then cp -s -f wsl-call.exe $file.exe; echo \"$file.exe = $fullname\"; fi; done");
+            cmd = winDir + "\\System32\\wsl.exe bash wsl-call.sh";
+            system(cmd.c_str());
+            return 0;
+        }
+        for( int i = 1; i < argc; i++ )
+        {
+            string name( lastNameInPath(argv[i]) );
+            writeFile( name + ".wslpath", argv[i] );
+            string c = cmd + "cp -s -f wsl-call.exe " + name + ".exe";
+            system( c );
+            printf("%s\n", argv[i]);
+        }
+        return 0;
+    }
+    cmd = cmd + wslPath(name);
+
+    for( int i = 1; i < argc; i++ )
+        cmd = cmd + " " + argv[i];
+
+    return system(cmd.c_str());
+}
